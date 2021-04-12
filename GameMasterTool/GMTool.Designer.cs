c56e0812579dd855969617b4612cc53314361fa2
Your Name <you@example.com>
2021-04-12 18:49:52 +0200

    Dodaj pliki projektów.

---
+++ GameMasterTool/GMTool.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace GameMasterTool
+{
+    partial class GMTool
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainButtonSession = new System.Windows.Forms.Button();
+            this.mainLabel = new System.Windows.Forms.Label();
+            this.mainButtonDiceRolls = new System.Windows.Forms.Button();
+            this.mainButtonMaps = new System.Windows.Forms.Button();
+            this.mainButtonMindMap = new System.Windows.Forms.Button();
+            this.mainButtonSettings = new System.Windows.Forms.Button();
+            this.mainButtonExit = new System.Windows.Forms.Button();
+            this.mainButtonAudio = new System.Windows.Forms.Button();
+            this.DoceRollsPanel = new System.Windows.Forms.Panel();
+            this.diceRollsLabel = new System.Windows.Forms.Label();
+            this.DoceRollsPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainButtonSession
+            // 
+            this.mainButtonSession.Cursor = System.Windows.Forms.Cursors.Default;
+            this.mainButtonSession.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.mainButtonSession.Location = new System.Drawing.Point(12, 51);
+            this.mainButtonSession.Name = "mainButtonSession";
+            this.mainButtonSession.Size = new System.Drawing.Size(281, 63);
+            this.mainButtonSession.TabIndex = 0;
+            this.mainButtonSession.Text = "Session";
+            this.mainButtonSession.UseVisualStyleBackColor = true;
+            this.mainButtonSession.Click += new System.EventHandler(this.mainButtonSession_Click);
+            // 
+            // mainLabel
+            // 
+            this.mainLabel.AutoSize = true;
+            this.mainLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.mainLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.mainLabel.Location = new System.Drawing.Point(12, 9);
+            this.mainLabel.Name = "mainLabel";
+            this.mainLabel.Size = new System.Drawing.Size(279, 39);
+            this.mainLabel.TabIndex = 1;
+            this.mainLabel.Text = "GameMasterTool";
+            // 
+            // mainButtonDiceRolls
+            // 
+            this.mainButtonDiceRolls.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.mainButtonDiceRolls.Location = new System.Drawing.Point(12, 120);
+            this.mainButtonDiceRolls.Name = "mainButtonDiceRolls";
+            this.mainButtonDiceRolls.Size = new System.Drawing.Size(281, 63);
+            this.mainButtonDiceRolls.TabIndex = 2;
+            this.mainButtonDiceRolls.Text = "Dice rolls";
+            this.mainButtonDiceRolls.UseVisualStyleBackColor = true;
+            this.mainButtonDiceRolls.Click += new System.EventHandler(this.mainButtonDiceRolls_Click);
+            // 
+            // mainButtonMaps
+            // 
+            this.mainButtonMaps.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.mainButtonMaps.Location = new System.Drawing.Point(12, 189);
+            this.mainButtonMaps.Name = "mainButtonMaps";
+            this.mainButtonMaps.Size = new System.Drawing.Size(281, 63);
+            this.mainButtonMaps.TabIndex = 3;
+            this.mainButtonMaps.Text = "Maps";
+            this.mainButtonMaps.UseVisualStyleBackColor = true;
+            // 
+            // mainButtonMindMap
+            // 
+            this.mainButtonMindMap.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.mainButtonMindMap.Location = new System.Drawing.Point(12, 258);
+            this.mainButtonMindMap.Name = "mainButtonMindMap";
+            this.mainButtonMindMap.Size = new System.Drawing.Size(281, 63);
+            this.mainButtonMindMap.TabIndex = 4;
+            this.mainButtonMindMap.Text = "Mind Map";
+            this.mainButtonMindMap.UseVisualStyleBackColor = true;
+            // 
+            // mainButtonSettings
+            // 
+            this.mainButtonSettings.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.mainButtonSettings.Location = new System.Drawing.Point(12, 396);
+            this.mainButtonSettings.Name = "mainButtonSettings";
+            this.mainButtonSettings.Size = new System.Drawing.Size(281, 63);
+            this.mainButtonSettings.TabIndex = 5;
+            this.mainButtonSettings.Text = "Settings";
+            this.mainButtonSettings.UseVisualStyleBackColor = true;
+            // 
+            // mainButtonExit
+            // 
+            this.mainButtonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.mainButtonExit.Location = new System.Drawing.Point(12, 465);
+            this.mainButtonExit.Name = "mainButtonExit";
+            this.mainButtonExit.Size = new System.Drawing.Size(281, 63);
+            this.mainButtonExit.TabIndex = 6;
+            this.mainButtonExit.Text = "Exit";
+            this.mainButtonExit.UseVisualStyleBackColor = true;
+            // 
+            // mainButtonAudio
+            // 
+            this.mainButtonAudio.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.mainButtonAudio.Location = new System.Drawing.Point(12, 327);
+            this.mainButtonAudio.Name = "mainButtonAudio";
+            this.mainButtonAudio.Size = new System.Drawing.Size(281, 63);
+            this.mainButtonAudio.TabIndex = 8;
+            this.mainButtonAudio.Text = "Audio";
+            this.mainButtonAudio.UseVisualStyleBackColor = true;
+            // 
+            // DoceRollsPanel
+            // 
+            this.DoceRollsPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DoceRollsPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.DoceRollsPanel.Controls.Add(this.diceRollsLabel);
+            this.DoceRollsPanel.Location = new System.Drawing.Point(299, 9);
+            this.DoceRollsPanel.Name = "DoceRollsPanel";
+            this.DoceRollsPanel.Size = new System.Drawing.Size(697, 516);
+            this.DoceRollsPanel.TabIndex = 9;
+            // 
+            // diceRollsLabel
+            // 
+            this.diceRollsLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.diceRollsLabel.AutoSize = true;
+            this.diceRollsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.diceRollsLabel.Location = new System.Drawing.Point(299, 8);
+            this.diceRollsLabel.Name = "diceRollsLabel";
+            this.diceRollsLabel.Size = new System.Drawing.Size(137, 31);
+            this.diceRollsLabel.TabIndex = 1;
+            this.diceRollsLabel.Text = "Dice Rolls";
+            // 
+            // GMTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1008, 537);
+            this.Controls.Add(this.DoceRollsPanel);
+            this.Controls.Add(this.mainLabel);
+            this.Controls.Add(this.mainButtonAudio);
+            this.Controls.Add(this.mainButtonSession);
+            this.Controls.Add(this.mainButtonMindMap);
+            this.Controls.Add(this.mainButtonExit);
+            this.Controls.Add(this.mainButtonDiceRolls);
+            this.Controls.Add(this.mainButtonMaps);
+            this.Controls.Add(this.mainButtonSettings);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MinimumSize = new System.Drawing.Size(1024, 576);
+            this.Name = "GMTool";
+            this.Text = "GMTool";
+            this.Resize += new System.EventHandler(this.GMTool_Resize);
+            this.DoceRollsPanel.ResumeLayout(false);
+            this.DoceRollsPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button mainButtonSession;
+        private System.Windows.Forms.Label mainLabel;
+        private System.Windows.Forms.Button mainButtonDiceRolls;
+        private System.Windows.Forms.Button mainButtonMaps;
+        private System.Windows.Forms.Button mainButtonAudio;
+        private System.Windows.Forms.Button mainButtonExit;
+        private System.Windows.Forms.Button mainButtonSettings;
+        private System.Windows.Forms.Button mainButtonMindMap;
+        private System.Windows.Forms.Panel DoceRollsPanel;
+        private System.Windows.Forms.Label diceRollsLabel;
+    }
+}
+
---
+++ GameMasterTool/GMTool.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameMasterTool
+{
+    public partial class GMTool : Form
+    {
+        int buttonOffset;
+        public GMTool()
+        {
+            InitializeComponent();
+            buttonOffset = 6;
+            //FormBorderStyle = FormBorderStyle.None; // Sizable None //
+            //WindowState = FormWindowState.Maximized; //Maximized Minimized Normal //
+
+            //**Ikona programu**//
+            this.Icon = new Icon("Resources/favicon.ico");
+        }
+
+        private void GMTool_Resize(object sender, EventArgs e)
+        {
+            // **Rozszerzanie sie przyciskow**//
+            mainButtonSession.Height = getNewButtonHeight();
+            // Pozycje przyciskow //
+            mainButtonDiceRolls.Height = getNewButtonHeight();
+            mainButtonDiceRolls.Location = new Point(mainButtonDiceRolls.Location.X, mainButtonSession.Height + buttonOffset + 51);
+
+            mainButtonMaps.Height = getNewButtonHeight();
+            mainButtonMaps.Location = new Point(mainButtonMaps.Location.X, (2 * (mainButtonSession.Height + buttonOffset)) + 51);
+
+            mainButtonMindMap.Height = getNewButtonHeight();
+            mainButtonMindMap.Location = new Point(mainButtonMindMap.Location.X, (3 * (mainButtonSession.Height + buttonOffset)) + 51);
+
+            mainButtonAudio.Height = getNewButtonHeight();
+            mainButtonAudio.Location = new Point(mainButtonAudio.Location.X, (4 * (mainButtonSession.Height + buttonOffset)) + 51);
+
+            mainButtonSettings.Height = getNewButtonHeight();
+            mainButtonSettings.Location = new Point(mainButtonSettings.Location.X, (5 * (mainButtonSession.Height + buttonOffset)) + 51);
+
+            mainButtonExit.Height = getNewButtonHeight();
+            mainButtonExit.Location = new Point(mainButtonExit.Location.X, (6 * (mainButtonSession.Height + buttonOffset)) + 51);
+        }
+        // Zmiana wysokości przyciskow
+        private int getNewButtonHeight()
+        {
+            return ((this.Height-90) / 7) - buttonOffset;
+        }
+
+        private void mainButtonSession_Click(object sender, EventArgs e)
+        {
+            DoceRollsPanel.Visible = false;
+        }
+        private void mainButtonDiceRolls_Click(object sender, EventArgs e)
+        {
+            DoceRollsPanel.Visible = true;
+        }
+
+        /*private void DontSaveExit_button(object sender, EventArgs e)
+{
+string message = "Do you want to quit without saving?";
+string title = "Quit";
+MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+DialogResult result = MessageBox.Show(message, title, buttons);
+if (result == DialogResult.Yes)
+{
+this.Close();
+}
+}*/
+    }
+}
